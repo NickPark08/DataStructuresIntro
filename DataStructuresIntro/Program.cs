2fc94805faac10256f550be401c67e6671943f77
Nicholas.Park <Nicholas.Park@GMR-118-1-3>
2023-07-10 17:31:35 -0700

    finished recursion intro, wip: MergeSort / making all sublists

--- DataStructuresIntro/Program.cs
+++ DataStructuresIntro/Program.cs
@@ -7,7 +7,6 @@ class Program
     public static void Main()
     {
         Recursion recursion = new Recursion();
-        int[] array = new int[] { 2, 323, 5 };
-        Console.WriteLine(recursion.SumArray(array));
+        recursion.IncreasingTriangle();
     }
 }
--- DataStructuresIntro/Recursion.cs
+++ DataStructuresIntro/Recursion.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace DataStructuresIntro
 {
-    class Recursion
+    class Recursion<T>
     {
         public int Countdown(int startNum, int endNum)
         {
             if (startNum < endNum) throw new ArgumentException("Cannot count up");
 
-            if(startNum == endNum)
+            if (startNum == endNum)
             {
                 return endNum;
             }
@@ -36,12 +37,72 @@ namespace DataStructuresIntro
 
         public string ReverseString(string word)
         {
-            return ReverseString(word);
+            return ReverseString(word, "", word.Length - 1);
         }
-        //strings are char arrays, word[0] = __
-        private string ReverseString(string word, string revWord)
+        private string ReverseString(string word, string revWord, int index)
         {
-            
+            if (index < 0) return revWord;
+
+            revWord += word[index];
+            return ReverseString(word, revWord, index - 1);
+        }
+
+        public string IncreasingTriangle()
+        {
+            return IncreasingTriangle(10, 1);
+        }
+        private string IncreasingTriangle(int spaces, int stars)
+        {
+            string triangle = "";
+            if (spaces < 0 && stars > 9) return triangle;
+
+            for (int i = 0; i < spaces; i++)
+            {
+                triangle += " ";
+            }
+            for (int i = 0; i < stars; i++)
+            {
+                triangle += "*";
+            }
+
+            Console.WriteLine(triangle);
+            return IncreasingTriangle(spaces - 1, stars + 2);
+        }
+
+        //confused on if function is void? 
+        //working on making all sublists
+        public List<T> MergeSort(List<T> list)
+        {
+            if (list.Count >= 2)
+            {
+                List<T> list1 = new List<T>();
+                List<T> list2 = new List<T>();
+
+                for (int i = 0; i < list.Count / 2; i++)
+                {
+                    list1.Add(list[i]);
+                }
+                for (int i = list.Count / 2; i < list.Count; i++)
+                {
+                    list2.Add(list[i]);
+                }
+
+                if (list1.Count >= 2)
+                {
+                    return MergeSort(list1);
+                }
+                if (list2.Count >= 2)
+                {
+                    return MergeSort(list2);
+                }
+            }
+            else return list;
+
+        }
+
+        public void Merge()
+        {
+
         }
     }
 }
