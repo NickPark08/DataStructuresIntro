7eef7bf4d9da714c6291bc051398be7578be11fb
Nicholas <Nicholas.Park@scsstudent.org>
2023-07-23 16:02:52 -0700

    wip: pop / heapify down

--- DataStructuresIntro/Program.cs
+++ DataStructuresIntro/Program.cs
@@ -6,15 +6,11 @@ class Program
 {
     public static void Main()
     {
-        RecursiveSorts<int> sorts= new RecursiveSorts<int>();
-        List<int> list = new List<int>() { 1, 2, 3, 4, 6 };
-        int[] array = new[] { 1, 5, 3, 7, 11, 9, 13, 2 }; 
-
-        sorts.QuickSortLomuto(array, 0, array.Length - 1);
-
-        foreach(int num in array)
-        {
-            Console.WriteLine(num); 
-        }
+        BinaryHeap<int> heap = new BinaryHeap<int>();
+        heap.Insert(10);
+        heap.Insert(20);
+        heap.Insert(30);
+        heap.Insert(12);
+        heap.Display();
     }
 }
