6e18e8ac5f697faf46b1ccb9ad2d3a29c5a83788
Nicholas <Nicholas.Park@scsstudent.org>
2023-07-29 20:06:13 -0700

    finished AVL tree node, wip: inserting

--- DataStructuresIntro/AVLTree.cs
+++ DataStructuresIntro/AVLTree.cs
@@ -6,15 +6,44 @@ using System.Threading.Tasks;
 
 namespace DataStructuresIntro
 {
-    class AVLTreeNode<T>
+    class AVLTreeNode<T> where T : IComparable<T>
     {
-        public AVLTreeNode(T value, AVLTreeNode<T> leftChild, AVLTreeNode<T> rightChild, int count, int height, int balance) 
+        T Value;
+        AVLTreeNode<T> LeftChild;
+        AVLTreeNode<T> RightChild;
+        int Height;
+        public AVLTreeNode(T value, AVLTreeNode<T> leftChild, AVLTreeNode<T> rightChild, int height) 
+        {
+            Value = value;
+            LeftChild = leftChild;
+            RightChild = rightChild;
+            Height = height;
+        }    
+        public int GetCount()
+        {
+            int count = 0;
+            if (LeftChild != null)
+            {
+                count++;
+            }
+            if (RightChild != null)
+            {
+                count++;
+            }
+            return count;
+        }
+        public int GetBalance()
+        {
+            return RightChild.Height - LeftChild.Height;
+        }
+    }
+    class AVLTree<T> where T : IComparable<T>
+    {
+        AVLTreeNode<T> Root;
+
+        public void Insert(T value)
         {
 
-        }    
-    }
-    class AVLTree<T>
-    {
-
+        }
     }
 }
