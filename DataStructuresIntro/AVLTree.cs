a56ea176f50af7b2378aa4d6b7e0461524f022bd
NickPark08 <nicholas.park@scsstudent.org>
2023-07-31 17:32:47 -0700

    finished insertion / adjusting height, wip: fix balancing and start double rotations

--- DataStructuresIntro/AVLTree.cs
+++ DataStructuresIntro/AVLTree.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,18 +9,22 @@ namespace DataStructuresIntro
 {
     class AVLTreeNode<T> where T : IComparable<T>
     {
-        T Value;
-        AVLTreeNode<T> LeftChild;
-        AVLTreeNode<T> RightChild;
-        int Height;
+        public T Value;
+        public AVLTreeNode<T> LeftChild;
+        public AVLTreeNode<T> RightChild;
+        public int Height;
         public AVLTreeNode(T value, AVLTreeNode<T> leftChild, AVLTreeNode<T> rightChild, int height) 
         {
             Value = value;
             LeftChild = leftChild;
             RightChild = rightChild;
             Height = height;
-        }    
-        public int GetCount()
+        }
+
+        public AVLTreeNode(T value)
+            : this(value, null, null, 1) { }
+
+        public int GetChildCount()
         {
             int count = 0;
             if (LeftChild != null)
@@ -34,16 +39,85 @@ namespace DataStructuresIntro
         }
         public int GetBalance()
         {
-            return RightChild.Height - LeftChild.Height;
+            if (RightChild == null && LeftChild == null) return 0;
+
+            else if(LeftChild == null) return RightChild.Height - 0;
+
+            else if(RightChild == null)return 0 - LeftChild.Height;
+
+            else return RightChild.Height - LeftChild.Height;
+
+        }
+        public int GetHeight()
+        {
+            if (LeftChild == null || (RightChild != null && LeftChild.Height < RightChild.Height))
+            {
+                Height = RightChild.Height + 1;
+            }
+            else
+            {
+                Height = LeftChild.Height + 1;
+            }
+            return Height;
         }
     }
     class AVLTree<T> where T : IComparable<T>
     {
-        AVLTreeNode<T> Root;
+        public AVLTreeNode<T> Root;
 
         public void Insert(T value)
         {
-
+            Root = Insert(value, Root);
         }
+        private AVLTreeNode<T> Insert(T value, AVLTreeNode<T> currentNode)
+        {
+            if (currentNode == null) return new(value);
+
+            if(value.CompareTo(currentNode.Value) < 0)
+            {
+                currentNode.LeftChild = Insert(value, currentNode.LeftChild);
+            }
+            else
+            {        
+                currentNode.RightChild = Insert(value, currentNode.RightChild);
+            }
+
+            if (currentNode.GetBalance() > 1)
+            {
+                //currentNode = currentNode.LeftChild;
+                return currentNode.RightChild;
+            }
+            else if (currentNode.GetBalance() < -1)
+            {
+                //currentNode = currentNode.RightChild;
+                return currentNode.LeftChild;
+            }
+
+            currentNode.GetHeight();
+            return currentNode;
+        }
+
+        //insertion reference
+
+        //public void BSTInsert(T value)
+        //{
+        //    Root = BSTInsert(value, Root);
+        //}
+        //private AVLTreeNode<T> BSTInsert(T value, AVLTreeNode<T> current)
+        //{
+        //    if (current == null) return new(value);
+
+        //    if (value.CompareTo(current.Value) < 0)
+        //    {
+        //        current.LeftChild = BSTInsert(value, current.LeftChild);
+        //    }
+        //    else
+        //    {
+        //        current.RightChild = BSTInsert(value, current.RightChild);
+        //    }
+        //    //if rotating, just return, the new subtree head
+        //    return current;
+        //}
+
     }
 }
--- DataStructuresIntro/Program.cs
+++ DataStructuresIntro/Program.cs
@@ -6,28 +6,13 @@ class Program
 {
     public static void Main()
     {
-        BinaryHeap<int> heap = new BinaryHeap<int>();
+        AVLTree<int> AVLTree = new AVLTree<int>();
 
-        List<int> list = new List<int>();
-        List<int> result = new List<int>();
+        AVLTree.Insert(10);
+        AVLTree.Insert(9);
+        AVLTree.Insert(8);
+        //AVLTree.Insert(7);
 
-        Random rnd = new Random();
-        for(int i = 0; i < 21; i++)
-        {
-            list.Add(rnd.Next(-100, 100));
-        }
-        for(int i = 0; i < list.Count; i++)
-        {
-            heap.Insert(list[i]);
-        }
-        for (int i = 0; i < list.Count - 1; i++)
-        {
-            result.Add(heap.Pop());
-        }
-
-        for (int i = 0; i < result.Count; i++)
-        {
-            Console.WriteLine(result[i]);
-        }
+        Console.WriteLine(AVLTree.Root.Value);
     }
 }
