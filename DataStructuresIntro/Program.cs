1936bd56ea5cbdda1257a9b6482fa1538cf46b55
Nicholas.Park <Nicholas.Park@GMR-118-2-3>
2023-09-29 17:35:54 -0700

    finished trie insertion, wip: remove

--- DataStructuresIntro/Program.cs
+++ DataStructuresIntro/Program.cs
@@ -1,33 +1,18 @@
 ﻿using DataStructuresIntro;
 
 using System.Data;
+using System.Reflection;
 
 class Program
 {
     public static void Main()
     {
-        SkipList<int> skipList = new SkipList<int>();
-        int[] ints = new int[10];
+        Trie trie = new Trie();
+        trie.Insert("hello");
+        trie.Insert("heyo");
+        trie.Insert("jazz");
 
-        skipList.Add(3);
-        skipList.Add(1);
-        skipList.Add(2);
-        skipList.Add(4);
-        skipList.Add(5);
-        skipList.Add(6);
-        skipList.Add(7);
-        skipList.Add(8);
-        skipList.Add(9);
-        skipList.Add(10);
-        skipList.Remove(5);
-        skipList.Remove(6);
-        skipList.Remove(2);
-        skipList.Remove(8);
-
-        foreach (int num in skipList)
-        {
-            Console.WriteLine(num);
-        }
+        TrieNode Pointer = trie.Root;
         ;
     }
 }
--- DataStructuresIntro/Trie.cs
+++ DataStructuresIntro/Trie.cs
@@ -1,18 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataStructuresIntro
 {
-    class TrieNode<T> where T : IComparable<T>
+    class TrieNode
     {
-        //char 
+        public char Letter { get; private set; }
+        public Dictionary<char, TrieNode> Children { get; private set; }
+        public bool IsWord { get; set; }
+
+        public TrieNode(char c)
+        {
+            Children = new Dictionary<char, TrieNode>();
+            Letter = c;
+            IsWord = false;
+        }
     }
 
-    class Trie<T> where T : IComparable<T>
+    class Trie
     {
+        public TrieNode Root = new TrieNode('$');
+        public void Insert(string word)
+        {
+            TrieNode Pointer = Root;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (!Pointer.Children.ContainsKey(word[i]))
+                {
+                    Pointer.Children.Add(word[i], new TrieNode(word[i]));
+                }
+                Pointer = Pointer.Children[word[i]];
+            }
+            Pointer.IsWord = true;
+        }
+        public bool Remove(string word)
+        {
+            TrieNode Pointer = Root;
 
+            if (!Pointer.Children.ContainsKey(word[0]))
+            {
+                return false;
+            }
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (Pointer.Children.ContainsKey(word[i]))
+                {
+                    Pointer = Pointer.Children[word[i]];
+                }
+            }
+            if ()
+            return true;
+        }
     }
 }
