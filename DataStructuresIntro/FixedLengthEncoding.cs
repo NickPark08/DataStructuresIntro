47500117fe00b174b7f75ec1e74666d54492dcc1
wanniepark <vinscully@outlook.com>
2024-02-12 07:00:27 -0800

    wip: fixed length encoding

--- DataStructuresIntro/FixedLengthEncoding.cs
+++ DataStructuresIntro/FixedLengthEncoding.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataStructuresIntro;
 
 namespace DataStructures
 {
@@ -12,10 +13,12 @@ namespace DataStructures
     {
         string File;
         PriorityQueue<HuffmanNode, int> Queue;
+        Dictionary<string, char> map;
         public FixedLengthEncoding(string file)
         {
             File = file;
             Queue = new PriorityQueue<HuffmanNode, int>();
+            map = new Dictionary<string, char>();
         }
 
         public void FillTree()
@@ -32,10 +35,47 @@ namespace DataStructures
                 Queue.Enqueue(node, node.Frequency);
 
             }
-            HuffmanNode left = Queue.Dequeue();
-            HuffmanNode right = Queue.Dequeue();
+            do
+            {
+                HuffmanNode left = Queue.Dequeue();
+                HuffmanNode right = Queue.Dequeue();
+                HuffmanNode parent = new (default, left, right, left.Frequency + right.Frequency);
 
-            HuffmanNode parent = new HuffmanNode(default, left, right, left.Frequency + right.Frequency);
+                Queue.Enqueue(parent, parent.Frequency);
+            } while (Queue.Count > 1);
+
+
+        }
+        private void MapChars()
+        {
+            Stack<HuffmanNode> stack = new Stack<HuffmanNode>();
+            Queue<char> queue = new Queue<char>();
+            var root = Queue.Peek();
+            string binary = "";
+
+            stack.Push(root);
+            while (stack.Count > 0)
+            {
+                var currentNode = stack.Pop();
+
+                queue.Enqueue(currentNode.Value);
+
+                if (currentNode.RightChild != null)
+                {
+                    stack.Push(currentNode.RightChild);
+                    binary += "1";
+                }
+                if (currentNode.LeftChild != null)
+                {
+                    stack.Push(currentNode.LeftChild);
+                    binary += "0";
+                }
+                if(currentNode.LeftChild == null && currentNode.RightChild == null)
+                {
+                    map.Add(binary, currentNode.Value);
+                    binary = "";
+                }
+            }
         }
     }
 }
