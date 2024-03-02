77394e49a782f76b572e81be7451ea951d423d83
Nicholas Park <nicholas.park@scsstudent.org>
2024-03-02 12:01:18 -0800

    finished huffman encoding, wip: B tree insert

---
+++ DataStructuresIntro/BTree.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace DataStructures
+{
+    public class BTree<T> where T : IComparable<T>
+    {
+        BTreeNode<T> root;
+
+        public void Insert(T value)
+        {
+            if(root == null)
+            {
+                root = new BTreeNode<T>(value);
+                return;
+            }
+            var currentNode = root;
+            while(currentNode.children.Count() != 0)
+            {
+                if (currentNode.values.Count() == 1)
+                {
+                    if (value.CompareTo(currentNode.values[0]) < 0)
+                    {
+                        currentNode = currentNode.children[0];
+                    }
+                    else
+                    {
+                        currentNode = currentNode.children[1];
+                    }
+                }
+                else if (currentNode.values.Count() == 2)
+                {
+                    if (value.CompareTo(currentNode.values[0]) < 0)
+                    {
+                        currentNode = currentNode.children[0];
+                    }
+                    else if (value.CompareTo(currentNode.values[0]) > 0 && value.CompareTo(currentNode.values[0]) < 0)
+                    {
+                        currentNode = currentNode.children[1];
+                    }
+                    else
+                    {
+                        currentNode = currentNode.children[2];
+                    }
+                }
+                else
+                { // finish if statements to traverse tree to add value
+                    else if (value.CompareTo(currentNode.values[1]) > 0 && value.CompareTo(currentNode.values[2]) < 0)
+                    {
+                        currentNode = currentNode.children[2];
+                    }
+                    else
+                    {
+                        currentNode = currentNode.children[3];
+                    }
+                }
+            }
+
+
+        }
+    }
+}
---
+++ DataStructuresIntro/BTreeNode.cs
@@ -0,0 +1,23 @@
+﻿using DataStructuresIntro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace DataStructures
+{
+    public class BTreeNode<T>
+    {
+        public List<BTreeNode<T>> children;
+        public List<T> values;
+
+        public BTreeNode(T v1)
+        {
+            values= new List<T>();
+            values.Add(v1);
+            children = new List<BTreeNode<T>>();
+        }
+    }
+}
